ba890a3c366d99721c6b9f3f82c16d54ac76e61d
Hao Chen <hche8093@uni.sydney.edu.au>
2023-01-16 11:00:42 +1100

    Add GraphQL config

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,27 +1,16 @@
-import { createContext, useState, useEffect } from 'react';
-
-import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+import { createContext, useState, useEffect } from 'react'
 
 export const CategoriesContext = createContext({
-  categoriesMap: {},
-});
+	categoriesMap: {},
+})
 
 export const CategoriesProvider = ({ children }) => {
-  const [categoriesMap, setCategoriesMap] = useState({});
+	const [categoriesMap, setCategoriesMap] = useState({})
 
-  useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categoryMap = await getCategoriesAndDocuments();
-      setCategoriesMap(categoryMap);
-    };
-
-    getCategoriesMap();
-  }, []);
-
-  const value = { categoriesMap };
-  return (
-    <CategoriesContext.Provider value={value}>
-      {children}
-    </CategoriesContext.Provider>
-  );
-};
+	const value = { categoriesMap }
+	return (
+		<CategoriesContext.Provider value={value}>
+			{children}
+		</CategoriesContext.Provider>
+	)
+}
